9f4f1997a9c3b5d7f6ae06ce2ebf1041c9fcc34d
Francis Carr <francis.carr@guardian.co.uk>
2013-09-30 18:08:24 +0100

    WIP

--- common/app/common/authentication.scala
+++ common/app/common/authentication.scala
@@ -90,3 +90,17 @@ class AuthAction(loginRoute: String) extends ExecutionContexts {
     }
   }
 }
+
+trait LoginController {
+  val openIdAttributes = Seq(
+    ("email", "http://axschema.org/contact/email"),
+    ("firstname", "http://axschema.org/namePerson/first"),
+    ("lastname", "http://axschema.org/namePerson/last")
+  )
+  val googleOpenIdUrl = "https://www.google.com/accounts/o8/id"
+
+  def login: Action[AnyContent]
+  def loginPost: Action[AnyContent]
+  def openIDCallback: Action[AnyContent]
+  def logout: Action[AnyContent]
+}

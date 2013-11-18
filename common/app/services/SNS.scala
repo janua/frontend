3415a3c65006d245c60d11c91e508910c8e4e6a3
Francis Carr <francis.carr@guardian.co.uk>
2013-11-18 21:36:10 +0000

    Add initial SNS object and trait

---
+++ common/app/services/SNS.scala
@@ -0,0 +1,53 @@
+package service
+
+import com.amazonaws.services.sns._
+import com.amazonaws.regions.{Region, Regions}
+import conf.Configuration
+import com.amazonaws.services.sns.model.{UnsubscribeRequest, ConfirmSubscriptionRequest, SubscribeRequest}
+import common.AkkaAgent
+
+trait SNS {
+
+  val agent = AkkaAgent[Option[String]](None)
+
+  val arn: String = Configuration.sns.faciaSns
+  val protocol: String = "http"
+  val endpoint: String = ""
+
+  def subscribe = {
+    val snsClient = new AmazonSNSClient(Configuration.aws.credentials)
+    snsClient.setRegion(Region.getRegion(Regions.EU_WEST_1))
+
+    snsClient.subscribe(new SubscribeRequest()
+      .withTopicArn(arn)
+      .withProtocol(protocol)
+      .withEndpoint(endpoint)
+    )
+  }
+
+  def unsubscribe = {
+    agent.get map { subscriptionId =>
+      val snsClient = new AmazonSNSClient(Configuration.aws.credentials)
+      snsClient.setRegion(Region.getRegion(Regions.EU_WEST_1))
+
+      snsClient.unsubscribe(
+        new UnsubscribeRequest()
+          .withSubscriptionArn(subscriptionId)
+      )
+    }
+  }
+
+  def confirmSubscription(token: String): Unit = {
+    val snsClient: AmazonSNSClient = new AmazonSNSClient(Configuration.aws.credentials)
+    snsClient.setRegion(Region.getRegion(Regions.EU_WEST_1))
+
+    val confirmSubscriptionResult = snsClient.confirmSubscription(
+      new ConfirmSubscriptionRequest()
+        .withToken(token)
+        .withTopicArn(arn)
+    )
+    agent.send(Option(confirmSubscriptionResult.getSubscriptionArn))
+  }
+}
+
+object SNS extends SNS

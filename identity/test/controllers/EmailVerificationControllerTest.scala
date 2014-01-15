705b9d3a3afcee8d99540835d73b78dffeffcde1
Nick-Smith <nicholas.smith@guardian.co.uk>
2014-01-15 17:55:43 +0000

    Fixed the resend validation email button. Added EmailVerificationControllerTest.

---
+++ identity/test/controllers/EmailVerificationControllerTest.scala
@@ -0,0 +1,12 @@
+package controllers
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: nsmith
+ * Date: 14/01/2014
+ * Time: 12:24
+ * To change this template use File | Settings | File Templates.
+ */
+class EmailVerificationControllerTest {
+
+}

437c21a277f352e7895c099085b8e3888901b9b0
chrisfinch <chrisfinchy@gmail.com>
2014-01-20 15:32:07 +0000

    Script now works with James G's endpoint and Nicks addidtions

--- common/app/assets/javascripts/modules/identity/validation-email.js
+++ common/app/assets/javascripts/modules/identity/validation-email.js
@@ -13,27 +13,40 @@ define([
     return {
         init: function (context) {
 
-            var resendButton = context.querySelector('.js-id-send-validation-email'),
-                $resendButton = bonzo(resendButton);
+            var resendButton = context.querySelector('.js-id-send-validation-email');
 
-            bean.on(resendButton, 'click', function (event) {
-                event.preventDefault();
+            console.log(context, resendButton);
 
-                IdentityApi.sendValidationEmail().then(
-                    function success(resp) {
-                        if (resp.status === 'error') {
-                            mediator.emit('module:identity:validation-email:fail');
-                        } else {
-                            mediator.emit('module:identity:validation-email:success');
-                            $resendButton.replaceWith("<p>Sent. Please check your email and follow the link.</p>");
-                        }
-                    },
-                    function fail(err, resp) {
-                        mediator.emit('module:identity:validation-email:fail');
-                        $resendButton.innerHTML = "Resend my verification email";
+            if (resendButton) {
+                
+                var $resendButton = bonzo(resendButton);
+
+                bean.on(resendButton, 'click', function (event) {
+                    event.preventDefault();
+
+                    if(IdentityApi.isUserLoggedIn()) {
+
+                        IdentityApi.sendValidationEmail().then(
+                            function success (resp) {
+                                console.log('suiccess', resp);
+                                if (resp.status === 'error') {
+                                    mediator.emit('module:identity:validation-email:fail');
+                                    $resendButton.innerHTML = "An error occured, please click here to try again.";
+                                } else {
+                                    mediator.emit('module:identity:validation-email:success');
+                                    $resendButton.replaceWith("<p>Sent. Please check your email and follow the link.</p>");
+                                }
+                            },
+                            function fail (err, resp) {
+                                console.log('err', err, resp);
+                                mediator.emit('module:identity:validation-email:fail');
+                                $resendButton.innerHTML = "An error occured, please click here to try again.";
+                            }
+                        );
+
                     }
-                );
-            });
+                });
+            }
         }
     };
 

9b3aa177062f0fc1de293594f36980da81bcb6e4
Francis Carr <francis.carr@guardian.co.uk>
2014-01-24 12:53:50 +0000

    Move over to facia-tool

--- facia-tool/app/Global.scala
+++ facia-tool/app/Global.scala
@@ -1,5 +1,8 @@
+import common.Jobs
 import java.io.File
+import jobs.FrontPressJob
 import play.api._
+import services.PorterConfigAgent
 
 object Global extends GlobalSettings {
 
@@ -9,4 +12,31 @@ object Global extends GlobalSettings {
     val newConfig: Configuration = if (mode == Mode.Dev) config ++ devConfig else config
     super.onLoadConfig(newConfig, path, classloader, mode)
   }
+
+  def scheduleJobs() {
+    Jobs.schedule("ConfigAgentJob", "0 * * * * ?") {
+      PorterConfigAgent.refresh()
+    }
+    Jobs.schedule("FrontPressJob", "0/5 * * * * ?") {
+      FrontPressJob.run()
+    }
+  }
+
+  def descheduleJobs() {
+    Jobs.deschedule("FrontPressJob")
+    Jobs.deschedule("ConfigAgentJob")
+  }
+
+  override def onStart(app: play.api.Application) {
+    super.onStart(app)
+    descheduleJobs()
+    scheduleJobs()
+    PorterConfigAgent.refresh()
+  }
+
+  override def onStop(app: play.api.Application) {
+    descheduleJobs()
+    super.onStop(app)
+  }
+
 }

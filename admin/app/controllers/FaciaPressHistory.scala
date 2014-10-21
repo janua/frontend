2137b8cc0aea26febc6809622cf7a417d41f5619
Francis Carr <francis.carr@guardian.co.uk>
2014-10-21 17:40:10 +0100

    Add FaciaPressHistory controller to facia-tool

---
+++ admin/app/controllers/FaciaPressHistory.scala
@@ -0,0 +1,21 @@
+package controllers.admin
+
+import play.api.libs.json.Json
+import play.api.mvc.{Action, Controller}
+import services.S3FrontsApi
+
+object FaciaPressHistory extends Controller {
+
+  def getHistoryFor(key: String) = Action {
+    Ok(Json.prettyPrint(Json.toJson(S3FrontsApi.getHistoryForPath(key))))
+  }
+
+  def getPressedHistoryFile(key: String, versionId: String) = Action {
+    Ok(Json.prettyPrint(Json.toJson(S3FrontsApi.getPressedHistoryVersion(key, versionId))))
+  }
+
+  def restoreVersion(key: String, versionId: String) = Action {
+    S3FrontsApi.restorePressedVersion(key, versionId)
+    Ok(s"Restored $key to $versionId")
+  }
+}

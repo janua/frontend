6246fa3c746c8f1f498fec894c5980807572f6fe
Francis Carr <francis.carr@guardian.co.uk>
2014-10-28 15:05:01 +0000

    Restore version returning Json and config endpoint for paths

--- admin/app/controllers/FaciaPressHistory.scala
+++ admin/app/controllers/FaciaPressHistory.scala
@@ -2,9 +2,13 @@ package controllers.admin
 
 import play.api.libs.json.Json
 import play.api.mvc.{Action, Controller}
-import services.S3FrontsApi
+import services.{ConfigAgent, S3FrontsApi}
 
 object FaciaPressHistory extends Controller {
+
+  def config = Action {
+    Ok(Json.toJson(ConfigAgent.getPathIds.sorted)).as("application/json")
+  }
 
   def getHistoryFor(key: String) = Action {
     Ok(Json.prettyPrint(Json.toJson(S3FrontsApi.getHistoryForPath(key)))).as("application/json")
@@ -16,6 +20,6 @@ object FaciaPressHistory extends Controller {
 
   def restoreVersion(key: String, versionId: String) = Action {
     S3FrontsApi.restorePressedVersion(key, versionId)
-    Ok(s"Restored $key to $versionId")
+    Ok(Json.toJson(s"Restored $key to $versionId"))
   }
 }

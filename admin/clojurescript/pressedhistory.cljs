3d89d134bb44c57906b7526e82b4dceac8474b2b
Francis Carr <francis.carr@guardian.co.uk>
2014-10-29 17:50:27 +0000

    Add all the clojurescript things

---
+++ admin/clojurescript/pressedhistory.cljs
@@ -0,0 +1,23 @@
+(ns guardian-frontend.pressedhistory
+  (:require [om.core :as om :include-macros true]
+            [om.dom :as dom :include-macros true]
+            [ajax.core :refer [GET POST]]
+            [guardian-frontend.components.paths :refer [pathsList]]
+            [guardian-frontend.components.versions :refer [versionsList]]
+            [guardian-frontend.components.utils :refer [updateVersionsForPath restorePath getPaths]]))
+
+(def app-state (atom {:paths []
+                      :currentPath {:path "No Selected Path" :versions []}}))
+
+(defn rootApp [data owner]
+  (reify
+    om/IRender
+    (render [this]
+      (apply dom/div #js {:className "flex-wrap"}
+      [(dom/h5 #js {:className "title"} "Restore History")
+       (om/build pathsList data)
+       (om/build versionsList (:currentPath data))]))))
+
+(om/root rootApp app-state {:target (. js/document (getElementById "mainapp"))})
+
+(getPaths (fn [r] (swap! app-state assoc :paths r)))

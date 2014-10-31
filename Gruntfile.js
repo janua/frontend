9e152021f22a3cd865401fa78cb140de9dfb73d5
Francis Carr <francis.carr@guardian.co.uk>
2014-10-31 17:29:54 +0000

    Remove cljs compiling from grunt for now

--- Gruntfile.js
+++ Gruntfile.js
@@ -103,8 +103,7 @@ module.exports = function (grunt) {
         'compile:fonts',
         'compile:flash',
         'asset_hash',
-        'compile:conf',
-        'compile:cljs'
+        'compile:conf'
     ]);
 
     /**

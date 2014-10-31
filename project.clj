c073fee66cb6f2cc721010803c7be82a3e589af5
Francis Carr <francis.carr@guardian.co.uk>
2014-10-31 17:31:35 +0000

    Remove licence

--- project.clj
+++ project.clj
@@ -1,8 +1,6 @@
 (defproject guardian-frontend "0.1.0-SNAPSHOT"
   :description "Guardian Frontend Clojurescript"
   :url "http://www.github.com/guardian/frontend"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-2342"]
                  [om "0.7.3"]

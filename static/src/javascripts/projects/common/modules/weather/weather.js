5b7049ee25eabd21ecc6f651ba4d98a3fc35b67f
Steve Vadocz <steve.vadocz@guardian.co.uk>
2014-11-20 14:47:48 +0000

    Added template. Making AJAX call.

--- static/src/javascripts/projects/common/modules/weather/weather.js
+++ static/src/javascripts/projects/common/modules/weather/weather.js
@@ -12,12 +12,50 @@ define([
     weatherTemplate
     ) {
 
-    function init() {
-
-    };
+    var self     = null,
+        $weather = null,
+        $holder  = null;
 
     return {
-        init: init
+        init: function () {
+            self = this;
+
+            $holder = $('.js-weather');
+
+            this.fetchData();
+        },
+
+        fetchData: function () {
+            var url = 'http://apidev.accuweather.com/currentconditions/v1/328328.json?apikey=3e74092c580e46319d36f04e68734365';
+            try {
+                ajax({
+                    url: url,
+                    type: 'json',
+                    method: 'get',
+                    crossOrigin: true
+                }).then(function (resp) {
+                    self.views.addToDOM(resp);
+                });
+            } catch (e) {
+                raven.captureException(new Error('Error retrieving weather data (' + e.message + ')'), {
+                    tags: {
+                        feature: 'weather'
+                    }
+                });
+            }
+        },
+
+        views: {
+            addToDOM: function (data) {
+                $weather = $.create(template(weatherTemplate, {
+                    location: 'London',
+                    icon: data['WeatherIcon'],
+                    tempNow: data['Temperature']['Metric']['Value']
+                }));
+
+
+            }
+        }
     };
 });
 

8f93596962b79be20c244aae91db9194245ba39c
Francis Carr <francis.carr@guardian.co.uk>
2015-09-08 14:54:20 +0100

    Change fbAppId to underscores

--- common/app/common/configuration.scala
+++ common/app/common/configuration.scala
@@ -222,7 +222,7 @@ class GuardianConfiguration(val application: String, val webappConfDirectory: St
   }
 
   object facebook {
-    lazy val appId = configuration.getMandatoryStringProperty("guardian.page.fbAppId")
+    lazy val appId = configuration.getMandatoryStringProperty("guardian_page_fbAppId")
     lazy val imageFallback = "http://static.guim.co.uk/icons/social/og/gu-logo-fallback.png"
   }
 
